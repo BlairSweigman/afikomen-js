448b9c823741f78652f653d69fdcf5cbddb3c3cd
BlairSweigman <blair.sweigman@gmail.com>
2020-04-07 22:54:51 -0400

    rounding and comments

--- js/app.js
+++ js/app.js
@@ -5,38 +5,68 @@
  */
 $(document).ready(function () {
     var afikomen;
-    var startTime =0;
+    var startTime = 0;
+
+    /**
+     * randomiz - sets afikomen to an integer between 1 and 10
+     *   */
     const randomize = () => {
         afikomen = Math.floor(Math.random() * 25) + 1;
     };
+
+
+
+    /** 
+     * showWin - hides the board, animates the Afikomen, and shows time
+     * 
+     * */
+    const showWin = () => {
+        MIDIjs.play('media/dayenu.mid');
+        $("#game").hide();
+        $("#header").empty().append("Mazel Tov! You found the Afikomen!");
+        $('#wingame').fadeIn(500).show();
+        animateIt();
+        const endTime = new Date().getTime();
+        const duration = (endTime - startTime) / 1000;
+        $("#time").empty().append(`You took ${duration.toFixed(2)} seconds.`);
+    };
+
+    /** hideBox - hides the box in a fadeout, resets clock on first box clicked
+     * 
+     * @param {int} id - the box to hide  
+     * 
+     *   */
+    const hideBox = (id) => {
+        $("#trig" + id).fadeOut(500);
+        if (startTime === 0) {
+            startTime = new Date().getTime();
+
+        }
+    };
+    /** 
+     *  setBpx - sets the onclick event for the box
+     *  if the box matches the afikomen, show the win, 
+     *  if not, hide the box
+     *  
+     *   @param (int) id - box to set 
+     * 
+     */
     const setBox = (id) => {
-     
         $("#trig" + id).click((e) => {
             e.preventDefault();
-          
             if (afikomen === id) {
-               
-                MIDIjs.play('media/dayenu.mid');
-                $("#game").hide();
-                
-                $("#header").empty().append("Mazel Tov! You found the Afikomen!");
-                $('#wingame').fadeIn(500).show();
-                animateIt();
-                const endTime = new Date().getTime();
-                const duration = (endTime-startTime)/1000;
-                $("#time").empty().append(`You took ${duration} seconds.`);
-                
+                showWin();
             } else {
-                $("#trig" +id).fadeOut(500)  ;  
-                if (startTime===0) {
-                    startTime = new Date().getTime();
-                   
-                }
+                hideBox(id);
             }
         });
     };
+
+    /**
+     * setBpxes - sets onlick event for all the boxes
+     * @returns {undefined}
+     */
     const setBoxes = () => {
-       
         for (let i = 1; i <= 25; i++) {
             setBox(i);
 
@@ -44,32 +74,53 @@ $(document).ready(function () {
 
 
     };
+    /**
+     * resetBoard - hides win,  clears timer,resets the game board, randomizes 
+     * the Afikomen
+     * @returns {undefined}
+     */
     const resetBoard = () => {
-        startTime=0;
-        for (let i=1; i<=25;i++) {
-            $("#trig"+i).show();
+        startTime = 0;
+        for (let i = 1; i <= 25; i++) {
+            $("#trig" + i).show();
         }
-         afikomen = Math.floor(Math.random() * 25) + 1;
-        $("#header").empty().append("Find the Afikomen")
-         $("#game").show();
+        randomize();
+        $("#header").empty().append("Find the Afikomen");
+        $("#game").show();
     };
+
+    /**
+     * onReset - stops music, hides win, resets afikomen position, resets board 
+     * @returns {undefined}
+     */
     const onReset = () => {
-        $("#reset").click((e)=>{
+        $("#reset").click((e) => {
             e.preventDefault();
             MIDIjs.stop();
             $("#wingame").hide();
-            $('#afi').css('transform','rotate(0deg)');
+            $('#afi').css('transform', 'rotate(0deg)');
             resetBoard();
-           
+
         });
     };
-    const animateIt=() =>{
-        
-        $('#afi').css('transform','rotate(135deg)');
-        
-    } ;
-    randomize();
-    setBoxes();
-    onReset();
+
+    /**
+     * animateIt - Spins the afikomen
+     * @returns {undefined}
+     */
+    const animateIt = () => {
+        $('#afi').css('transform', 'rotate(135deg)');
+    };
+/**
+ * Main - randomizes Afikomen, sets put board, sets up reset button
+ * @returns {undefined}
+ */
+    const main = () => {
+        randomize();
+        setBoxes();
+        onReset();
+    };
+
+    main();
 });
 
